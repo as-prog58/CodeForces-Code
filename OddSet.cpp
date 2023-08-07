8ac07e0ad78117d4be78f6cef049bba9268ed7f4
as-prog58 <131233333+as-prog58@users.noreply.github.com>
2023-08-07 09:14:31 +0530

    code

---
+++ OddSet.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h>
+// #include "utilities.cpp"
+using namespace std;
+
+
+
+
+const int mod = 1e9 + 7;
+// const int mod = 998244353;
+
+
+
+
+
+signed main()
+{
+
+    
+    int t = 1;
+    cin >> t;
+    while (t--) 
+	{
+	    
+	}
+	return 0;
+	
+}
+    
+
